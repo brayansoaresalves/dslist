58b3b2131e01805e9b66a8de8664e1c0db28517f
brayansoaresalves <brayan.iub10@gmail.com>
2023-05-11 22:18:02 -0300

    implementando metodo de trocar posição

--- dslist/src/main/java/com/devsuperior/dslist/services/GameListService.java
+++ dslist/src/main/java/com/devsuperior/dslist/services/GameListService.java
@@ -4,10 +4,13 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.devsuperior.dslist.dto.GameListDTO;
 import com.devsuperior.dslist.entities.GameList;
+import com.devsuperior.dslist.projections.GameMinProjection;
 import com.devsuperior.dslist.repositories.GameListRepository;
+import com.devsuperior.dslist.repositories.GameRepository;
 
 @Service
 public class GameListService {
@@ -15,9 +18,27 @@ public class GameListService {
 	@Autowired
 	private GameListRepository listRepository;
 	
+	@Autowired
+	private GameRepository repository;
+	
 	public List<GameListDTO> buscarTodos(){
 		List<GameList> lista = listRepository.findAll();
 		return lista.stream().map(x -> new GameListDTO(x)).toList();
+	}
+	
+	@Transactional
+	public void move(Long listId, int sourceIndex, int destinationIndex) {
+		List<GameMinProjection> result = repository.searchByList(listId);
+		
+		GameMinProjection obj = result.remove(sourceIndex);
+		result.add(destinationIndex, obj);
+		
+		int min = sourceIndex < destinationIndex ? sourceIndex : destinationIndex;
+		int max = sourceIndex < destinationIndex ? destinationIndex : sourceIndex;
+		
+		for (int i = min; i<=max; i++) {
+			listRepository.updateBelongingPosition(listId, result.get(i).getId(), i);
+		}
 	}
 
 }
